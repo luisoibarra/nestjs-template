d27d5b61ac5c176ec1a3b4f85b3aa2faf164ba9b
Luis Ibarra <luise98cu@gmail.com>
2024-07-01 07:32:37 -0400

    Final Initial Database

--- prisma/migrations/20240701113051_init/migration.sql
+++ prisma/migrations/20240701113051_init/migration.sql
@@ -1,18 +1,19 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(191) NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `passwordHash` VARCHAR(191) NOT NULL,
+    `emailVerified` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-  - You are about to drop the column `name` on the `user` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[userName]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `name`,
-    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `emailVerified` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `firstName` VARCHAR(191) NULL,
-    ADD COLUMN `lastName` VARCHAR(191) NULL,
-    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `userName` VARCHAR(191) NULL;
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_userName_key`(`userName`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Token` (
@@ -33,13 +34,10 @@ CREATE TABLE `EmailTemplate` (
     `id` VARCHAR(191) NOT NULL,
     `templateName` VARCHAR(191) NOT NULL,
     `subject` VARCHAR(191) NOT NULL,
-    `template` VARCHAR(191) NOT NULL,
+    `template` LONGTEXT NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateIndex
-CREATE UNIQUE INDEX `User_userName_key` ON `User`(`userName`);
 
 -- AddForeignKey
 ALTER TABLE `Token` ADD CONSTRAINT `Token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
